65b0a386e8ab74ee49c41aaf78af35236ef28aad
belmu <belmuledozo@gmail.com>
2024-12-27 22:16:05 +0100

    Added Main.hpp

---
+++ src/Main.hpp
@@ -0,0 +1,16 @@
+#ifndef MAIN_HPP
+#define MAIN_HPP
+
+// Game constants
+static constexpr int    WINDOW_WIDTH     = 560;
+static constexpr int    WINDOW_HEIGHT    = 900;
+static constexpr double TARGET_FRAMERATE = 60;
+
+static constexpr float PLATE_SPEED = 10.0;
+
+static constexpr int BRICK_ROWS    = 8;
+static constexpr int BRICK_COLUMNS = 14;
+static constexpr int BRICK_WIDTH   = 40;
+static constexpr int BRICK_HEIGHT  = 50;
+
+#endif
