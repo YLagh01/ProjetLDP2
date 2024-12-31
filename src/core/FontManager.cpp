d80e917d8b9329f94d76ecc404b584df03fa0037
Belmu <36384689+BelmuTM@users.noreply.github.com>
2024-12-31 02:05:12 +0100

    Update FontManager.cpp

--- src/core/FontManager.cpp
+++ src/core/FontManager.cpp
@@ -1,8 +1,5 @@
 #include "FontManager.hpp"
 
-#include <iostream>
-#include <ostream>
-
 FontManager::FontManager() {
     main_font      = al_load_font("../res/fonts/press_start_2p.ttf", MAIN_FONT_SIZE, 0);
     secondary_font = al_load_font("../res/fonts/retro_gaming.ttf", MAIN_FONT_SIZE, 0);
